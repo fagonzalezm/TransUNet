123ebe7d9bc5c217d74a786cbec020cdbbd915be
fagonzalezm <felipe.gonzalez.me@usach.cl>
2022-05-30 02:29:10 -0400

    HF/ remove pretrain

--- train.py
+++ train.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     if args.vit_name.find('R50') != -1:
         config_vit.patches.grid = (int(args.img_size / args.vit_patches_size), int(args.img_size / args.vit_patches_size))
     net = ViT_seg(config_vit, img_size=args.img_size, num_classes=config_vit.n_classes).cuda()
-    net.load_from(weights=np.load(config_vit.pretrained_path))
+    # net.load_from(weights=np.load(config_vit.pretrained_path))
 
     trainer = {'SCIAN-SpermSegGS-Original': trainer_scian,}
     trainer[dataset_name](args, net, snapshot_path)
